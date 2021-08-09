8fd9c372a4a8785da1fa56ba4ffd704316907a65
YfNightWind <lyh1147557099@outlook.com>
2021-08-09 19:54:55 +0800

    📌

---
+++ src/main/java/com/alexlin/yangocanteen/YangoCanteenApplication.java
@@ -0,0 +1,13 @@
+package com.alexlin.yangocanteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangoCanteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangoCanteenApplication.class, args);
+    }
+
+}
