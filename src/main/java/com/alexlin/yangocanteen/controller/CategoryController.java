7f3f7bf5038e133e4469e39570223dd33c805b19
YfNightWind <lyh1147557099@outlook.com>
2021-08-25 01:00:59 +0800

    have some bugs

---
+++ src/main/java/com/alexlin/yangocanteen/controller/CategoryController.java
@@ -0,0 +1,37 @@
+package com.alexlin.yangocanteen.controller;
+
+import com.alexlin.yangocanteen.base.BaseResult;
+import com.alexlin.yangocanteen.bean.Category;
+import com.alexlin.yangocanteen.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryMapper mapper;
+
+    @GetMapping("/get")
+    public BaseResult findAll() {
+        List<Category> categories = mapper.findAll();
+        if (categories == null) {
+            return new BaseResult(500, "未查询到数据", "");
+        } else {
+            return new BaseResult(200, "成功", categories);
+        }
+    }
+
+    @PostMapping("/getmenu")
+    public BaseResult findByMenu(@RequestParam(value = "category", defaultValue = "") String category) {
+
+        if (category.equals("")) return new BaseResult(400, "参数不能为空", "");
+        List<Category> all = mapper.findCategoryFromMenu(category);
+
+        return new BaseResult(200, "查询成功", all);
+
+    }
+
+}
---
+++ src/main/java/com/alexlin/yangocanteen/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.alexlin.yangocanteen.mapper;
+
+import com.alexlin.yangocanteen.bean.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+    // 查询所有分类
+    @Select("SELECT NAME 'category' FROM category;")
+    List<Category> findAll();
+
+    // 从菜单中查找分类 ‼️
+    @Select("SELECT * FROM menu WHERE category = #{category}")
+    List<Category> findCategoryFromMenu (String category);
+}
