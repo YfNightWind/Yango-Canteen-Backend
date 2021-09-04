8070170d463b565de33ab2f7ea7c5cc5df403f77
YfNightWind <lyh1147557099@outlook.com>
2021-09-04 16:50:41 +0800

    完成文件上传功能🎈

--- src/main/java/com/alexlin/yangocanteen/controller/UploadFileController.java
+++ src/main/java/com/alexlin/yangocanteen/controller/UploadFileController.java
@@ -1,2 +1,25 @@
-package com.alexlin.yangocanteen.controller;public class FileController {
+package com.alexlin.yangocanteen.controller;
+
+import com.alexlin.yangocanteen.result.BaseResult;
+import com.alexlin.yangocanteen.util.FileUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("upload")
+public class UploadFileController {
+    @PostMapping
+    public BaseResult upload(@RequestParam("image") MultipartFile image) throws Exception {
+
+        String path = "";// 路径待确定
+        String result = FileUtil.upload(image,path, Objects.requireNonNull(image.getOriginalFilename()));
+
+        return new BaseResult(200,"上传成功！",result);
+    }
 }
